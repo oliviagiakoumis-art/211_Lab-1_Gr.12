d4514689950a0b767b53fc940639b2fc0fb88356
Zydd04 <ziyad.elfettahi@mail.mcgill.ca>
2026-09-22 10:48:33 -0400

    Update bang

--- Documents/ecse211-code/labs/bang_bang.py
+++ Documents/ecse211-code/labs/bang_bang.py
@@ -6,25 +6,27 @@ rightmotor = Motor("A")
 
 us_sensor = EV3UltrasonicSensor(1)
 
-BAND_WIDTH = 7
+BAND_WIDTH = 3
 BAND_CENTER = 20
 
 if __name__ == "__main__":
     wait_ready_sensors()
     try:
-        d = us_sensor.get_cm()
-
-        while(d < BAND_CENTER - BAND_WIDTH):
-            rightmotor.set_dps(20)
-            leftmotor.set_dps(10)
-            time.sleep(0.01)
+        while(True):
             d = us_sensor.get_cm()
 
-        while(d > BAND_CENTER + BAND_WIDTH):
-            leftmotor.set_dps(20)
-            rightmotor.set_dps(10)
+            if (d < BAND_CENTER - BAND_WIDTH):
+                rightmotor.set_dps(20)
+                leftmotor.set_dps(10)
+            
+            elif (d > BAND_CENTER + BAND_WIDTH):
+                leftmotor.set_dps(20)
+                rightmotor.set_dps(10)
+            else:
+                rightmotor.set_dps(20)
+                leftmotor.set_dps(20)
             time.sleep(0.01)
-            d = us_sensor.get_cm()
+            
 
     except BaseException:
         reset_brick()
