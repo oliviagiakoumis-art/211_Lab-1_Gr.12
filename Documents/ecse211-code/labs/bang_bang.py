c4081f15319def76405e3f25db3524b0adf63395
Zydd04 <ziyad.elfettahi@mail.mcgill.ca>
2026-09-24 11:42:47 -0400

    encoder fix

--- Documents/ecse211-code/labs/bang_bang.py
+++ Documents/ecse211-code/labs/bang_bang.py
@@ -8,20 +8,29 @@ us_sensor = EV3UltrasonicSensor(1)
 
 BAND_WIDTH = 3
 BAND_CENTER = 20
-
+def rotate(t) :
+    initial = rightmotor.get_encoder()
+    final = initial + 90
+    while(rightmotor.get_encoder() < final):
+        rightmotor.set_dps(t)
+        leftmotor.set_dps(-t)
+        time.sleep(0.01)
 if __name__ == "__main__":
     wait_ready_sensors()
     try:
         while(True):
             d = us_sensor.get_cm()
-
-            if (d < BAND_CENTER - BAND_WIDTH):
+            if (d >= 60 and d <= 255):
+                rotate(90)
+                
+            elif (d < BAND_CENTER - BAND_WIDTH):
                 rightmotor.set_dps(20)
                 leftmotor.set_dps(10)
             
             elif (d > BAND_CENTER + BAND_WIDTH):
                 leftmotor.set_dps(20)
                 rightmotor.set_dps(10)
+
             else:
                 rightmotor.set_dps(20)
                 leftmotor.set_dps(20)
@@ -31,5 +40,3 @@ if __name__ == "__main__":
     except BaseException:
         reset_brick()
         exit()
-
-    reset_brick()
